527bc8ac4ab6a3e8585461645c81ec99f924e68c
gregl83 <general@gregorylanglais.com>
2013-11-20 00:35:19 -0500

    Added Regular Expression Support

--- test/mongoscope-literal-instance.test.js
+++ test/mongoscope-literal-instance.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var util = require('util');
 var assert = require('assert');
 
 var mongoScope = require('../');
@@ -37,7 +38,7 @@ var objectLiteral = {
   d1: new Date(null) // date object (epoc)
 };
 
-var expected = '{"b1":true,"b2":false,"s1":"string","s2":"string","i1":1,"i2":-5,"fl1":1.5,"fl2":0.5,"fl3":-1.25,"fu1":function (){return false},"fu2":function i(){return true},"a1":[0,1],"a2":[0,1],"o1":{},"o2":{},"o3":{"name":"nested literal","nested":true},"r1":{},"r2":{},"d1":new Date("1970-01-01T00:00:00.000Z")}';
+var expected = '{"b1":true,"b2":false,"s1":"string","s2":"string","i1":1,"i2":-5,"fl1":1.5,"fl2":0.5,"fl3":-1.25,"fu1":function (){return false},"fu2":function i(){return true},"a1":[0,1],"a2":[0,1],"o1":{},"o2":{},"o3":{"name":"nested literal","nested":true},"r1":/^abc$/i,"r2":/zxy/,"d1":new Date("1970-01-01T00:00:00.000Z")}';
 
 describe('mongoScope', function() {
   it('should construct valid scope function when given object literal', function(done) {
@@ -50,9 +51,12 @@ describe('mongoScope', function() {
     var evaluate = '(' + expected + ')';
     var evaluated = eval(evaluate);
 
-    // fixme regular expressions don't work
+    var inspectOptions =  {showHidden: true, depth: null};
 
-    assert.deepEqual(evaluated, objectLiteral);
+    var evaluatedInspect = util.inspect(evaluated, inspectOptions);
+    var objectLiteralInspect = util.inspect(objectLiteral, inspectOptions);
+
+    assert.strictEqual(evaluatedInspect, objectLiteralInspect);
 
     done();
   });
